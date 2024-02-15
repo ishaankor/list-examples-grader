3aecbe3b692ebafbe9dde8607fba8db370a5ece8
ishaankor <113160688+ishaankor@users.noreply.github.com>
2024-02-15 09:11:31 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -8,6 +8,23 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if ! [ -f student-submission/ListExamples.java ]
+then 
+    echo "Missing files!"
+    exit 
+fi
+    echo "Found files!"
+
+cp student-submission/ListExamples.java TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+javac -cp $CPATH *.java 2> output
+if [ $? -ne 0 ]
+then
+    echo "Compilation error!"
+    cat output
+fi
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
